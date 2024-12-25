84d6d8423693645ea2a515bc1bfddf100ba568aa
asiripr <asiripramodaya2000@gmail.com>
2024-12-25 18:06:55 +0530

    improved homepage

--- project-root/lms-frontend/src/App.jsx
+++ project-root/lms-frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Navigate, Routes } from 'react-router-dom';
-import Home from './pages/DashBoardPage';
+import DashBoardPage from './pages/DashBoardPage';
 // import Login from './components/auth/Login';
 // import NotFound from './components/error/NotFound';
+
 
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/home' />}></Route>
-      <Route path='/home' element={<Home />}></Route>
+      <Route path='/home' element={<DashBoardPage />}></Route>
       {/* <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} /> */}
     </Routes>
---
+++ project-root/lms-frontend/src/components/common/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© 2024 Library Management System</p>
+        </footer>
+    );
+};
+export default Footer;
