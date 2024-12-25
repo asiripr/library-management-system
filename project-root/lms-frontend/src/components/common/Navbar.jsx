1132ee6746ef800c24bb1f81ed7cc196d6ede459
asiripr <asiripramodaya2000@gmail.com>
2024-12-25 17:06:53 +0530

    navbar

---
+++ project-root/lms-frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-links">
+                <a href="/profile">Profile</a>
+                <button>Logout</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
