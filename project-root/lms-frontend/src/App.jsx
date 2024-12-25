eb8ecda35ee8e7ea7d4dbab2d71b05e5f2e08f95
asiripr <asiripramodaya2000@gmail.com>
2024-12-25 13:21:33 +0530

    added main routes

--- project-root/lms-frontend/src/App.jsx
+++ project-root/lms-frontend/src/App.jsx
@@ -1,4 +1,17 @@
-import { Router, Route, Navigate } from 'react-router-dom';
+import { Router, Route, Navigate, Routes } from 'react-router-dom';
 import Home from './pages/DashBoardPage';
 import Login from './components/auth/Login';
 import NotFound from './components/error/NotFound';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to={'/home'} />}></Route>
+      <Route path='/home' element={<Home />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
+  );
+};
+
+export default App;
--- project-root/lms-frontend/src/pages/DashBoardPage.jsx
+++ project-root/lms-frontend/src/pages/DashBoardPage.jsx
@@ -1,1 +1,2 @@
 // this is our main home page
+
